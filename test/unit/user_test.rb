928b8261da6d12a944003c1520973ab20455f3ac
Stephen Sizer <stevey@twinly.co.uk>
2013-04-21 18:01:14 +0100

    Fixed bug with user profile validation

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -29,11 +29,21 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "a user should have a profile name without spaces" do
-    user = User.new
+    user = User.new(first_name: 'Steve', last_name: 'Sizer', email: 'stevey@twinly.co.uk')
+    user.password = user.password_confirmation = 'asdfasfd'
+    
     user.profile_name = "My Profile With Spaces"
 
     assert !user.save
     assert !user.errors[:profile_name].empty?
     assert user.errors[:profile_name].include?("Must be formatted correctly.")
+  end
+
+  test "a user can have a correctly formatted profile name" do
+    user = User.new(first_name: 'Steve', last_name: 'Sizer', email: 'stevey@twinly.co.uk')
+    user.password = user.password_confirmation = 'asdfasfd'
+
+    user.profile_name = 'steveysizer_1'
+    assert user.valid?
   end
 end
